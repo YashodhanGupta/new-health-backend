a74c47b2a73e5f29af9318a6bb000b1969fe483f
Yashodhan Gupta <yashodhan1273@gmail.com>
2025-04-01 12:02:48 +0530

    just after success deploy

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,6 @@
 import User from "../models/UserSchema.js";
 import Doctor from "../models/DoctorSchema.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 const generateToken = (user) => {
